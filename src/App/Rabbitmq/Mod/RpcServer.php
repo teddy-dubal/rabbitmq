21672390936d6785d7fb6d878806b353b08895bd
Teddy Dona <teddy.dubal@gmail.com>
2019-08-17 19:52:27 -0400

    * fix

--- src/App/Rabbitmq/Mod/RpcServer.php
+++ src/App/Rabbitmq/Mod/RpcServer.php
@@ -68,8 +68,10 @@ class RpcServer
             ->push('Swarrot\Processor\ExceptionCatcher\ExceptionCatcherProcessor', $this->logger)
             ->push('Swarrot\Processor\Ack\AckProcessor', $messageProvider, $this->logger)
         ;
-        $callback->setDic($this->_dic);
-        $processor = $stack->resolve(new RpcServerProcessor(new $callback(), $messagePub, $this->logger));
+        $cb = new $callback();
+        $cb->setDic($this->_dic);
+
+        $processor = $stack->resolve(new RpcServerProcessor($cb, $messagePub, $this->logger));
         $consumer  = new SConsumer($messageProvider, $processor, null, $this->logger);
         $consumer->consume([]);
     }
