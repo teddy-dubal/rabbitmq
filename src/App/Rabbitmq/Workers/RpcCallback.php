12519b12543c178d2ce769551689318905082a2f
Teddy Dona <teddy.dubal@gmail.com>
2019-08-17 18:45:29 -0400

    * improve rpc callback

--- src/App/Rabbitmq/Workers/RpcCallback.php
+++ src/App/Rabbitmq/Workers/RpcCallback.php
@@ -5,7 +5,7 @@ namespace App\Rabbitmq\Workers;
 use Swarrot\Broker\Message;
 use Swarrot\Processor\ProcessorInterface;
 
-class rpcCallback implements ProcessorInterface
+class RpcCallback implements ProcessorInterface
 {
 
     private $_dic;
@@ -16,22 +16,13 @@ class rpcCallback implements ProcessorInterface
         $dic    = $this->_dic;
         $result = json_encode(['status' => 0, 'result' => ['error' => 'unknow_callback']]);
         $action = explode('::', $body['client.call_action']);
-        $dic['log']->addNotice($body['client.call_action'] . json_encode($body));
+        $this->_dic['log']->debug($body['client.call_action'], $body);
         if (class_exists($action[0]) && method_exists($action[0], $action[1])) {
             $time_start = microtime(true);
-            $object     = new $action[0]($dic);
+            $object     = new $action[0]($this->_dic);
             $result     = $object->{$action[1]}($body);
             $time       = microtime(true) - $time_start;
-            $dic['log']->addNotice(sprintf('action %s in %s => %s', $body['client.call_action'], strval($time), $result));
-            if ($dic['debug']) {
-                $dt = date('Y-m-d H:i:s');
-                echo '*-------------' . $dt . '---------------* ' . PHP_EOL;
-                echo $body['client.call_action'] . PHP_EOL;
-                var_dump($body);
-                echo '**** ' . PHP_EOL;
-                var_dump(json_decode($result, true));
-                echo '*-------------' . $dt . '---------------* ' . PHP_EOL;
-            }
+            $this->_dic['log']->debug(sprintf('[RCP-CALLBACK] action %s in %s => %s', $body['client.call_action'], strval($time), json_decode($result, true)));
         }
         return $result;
     }
